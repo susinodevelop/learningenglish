69967b673cdeda6dd149c13b42b90c4983644b12
Jesús Fernández <119144835+susinodevelop@users.noreply.github.com>
2026-09-04 21:33:21 +0200

    feat: complete B2 grammar fidelity from study book
    * feat: restore omitted B2 grammar details from study book
    
    * feat: apply complete B2 grammar source fidelity

--- lib/grammar/source.ts
+++ lib/grammar/source.ts
@@ -2,10 +2,13 @@ import { grammarUnits01to06 } from "./source/book-part-a";
 import { grammarUnits07to12 } from "./source/book-part-b";
 import { grammarUnits13to18 } from "./source/book-part-c";
 import { grammarUnits19to24 } from "./source/book-part-d";
+import { applyBookFidelityPatches } from "./source/book-fidelity";
 
-export const bookGrammarTopics = [
+const rawBookGrammarTopics = [
   ...grammarUnits01to06,
   ...grammarUnits07to12,
   ...grammarUnits13to18,
   ...grammarUnits19to24,
 ];
+
+export const bookGrammarTopics = applyBookFidelityPatches(rawBookGrammarTopics);
