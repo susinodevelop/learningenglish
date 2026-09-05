769a114bc3546439641a12a4adb25871d033e2d9
Jesús Fernández <119144835+susinodevelop@users.noreply.github.com>
2026-09-05 02:31:28 +0200

    feat: compile pedagogical English definitions

---
+++ lib/vocabulary/pedagogy/index.ts
@@ -0,0 +1,24 @@
+import { cultureGlosses } from "./culture-glosses";
+import { makeGlossMap, glossKey } from "./glosses";
+import { peopleGlosses } from "./people-glosses";
+import { placesGlosses } from "./places-glosses";
+import { studyWorkGlosses } from "./study-work-glosses";
+import { timeGlosses } from "./time-glosses";
+import { worldGlosses } from "./world-glosses";
+
+const glossMap = makeGlossMap([
+  worldGlosses,
+  peopleGlosses,
+  cultureGlosses,
+  placesGlosses,
+  timeGlosses,
+  studyWorkGlosses,
+]);
+
+export function getEnglishDefinition(term: string, meaningEs: string) {
+  return glossMap.get(glossKey(term, meaningEs));
+}
+
+export function getVocabularyGlossKeys() {
+  return new Set(glossMap.keys());
+}
