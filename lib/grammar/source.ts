2b2b94e08bb61dd46fd3a1dfe3d2771e55638295
Jesús Fernández <119144835+susinodevelop@users.noreply.github.com>
2026-09-07 13:24:26 +0200

    feat: include Gold C1 grammar source

--- lib/grammar/source.ts
+++ lib/grammar/source.ts
@@ -3,12 +3,26 @@ import { grammarUnits07to12 } from "./source/book-part-b";
 import { grammarUnits13to18 } from "./source/book-part-c";
 import { grammarUnits19to24 } from "./source/book-part-d";
 import { applyBookFidelityPatches } from "./source/book-fidelity";
+import { c1GoldGrammarUnits01to03 } from "./source/c1-gold-part-a";
+import { c1GoldGrammarUnits04to06 } from "./source/c1-gold-part-b";
+import { c1GoldGrammarUnits07to09 } from "./source/c1-gold-part-c";
+import { c1GoldGrammarUnits10to12 } from "./source/c1-gold-part-d";
 
-const rawBookGrammarTopics = [
+const rawB2BookGrammarTopics = [
   ...grammarUnits01to06,
   ...grammarUnits07to12,
   ...grammarUnits13to18,
   ...grammarUnits19to24,
 ];
 
-export const bookGrammarTopics = applyBookFidelityPatches(rawBookGrammarTopics);
+const c1GoldGrammarTopics = [
+  ...c1GoldGrammarUnits01to03,
+  ...c1GoldGrammarUnits04to06,
+  ...c1GoldGrammarUnits07to09,
+  ...c1GoldGrammarUnits10to12,
+];
+
+export const bookGrammarTopics = [
+  ...applyBookFidelityPatches(rawB2BookGrammarTopics),
+  ...c1GoldGrammarTopics,
+];
